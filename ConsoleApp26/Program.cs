1f10e6df92c75fc8a8542c46b06f011c2ee0e4ed
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-02-17 04:26:43 +0300

    Linq Deleting

--- ConsoleApp26/Program.cs
+++ ConsoleApp26/Program.cs
@@ -12,49 +12,14 @@ namespace ConsoleApp26
         {
             VTContext vt = new VTContext();
 
-            var urunler = vt.Urunler.ToList();
+            var kategori = vt.Kategoriler.Where(i => i.Id == 1).FirstOrDefault();
 
-            foreach (var urun in urunler)
+            if(kategori != null)
             {
-                Console.WriteLine(urun.Fiyat);
-            }
-
-            foreach (var urun in urunler)
-            {
-                urun.Fiyat *= 1.25;
+                vt.Kategoriler.Remove(kategori);
             }
             vt.SaveChanges();
 
-            Console.WriteLine("------------------------------------");
-
-            foreach (var urun in urunler)
-            {
-                Console.WriteLine(urun.Fiyat);
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-            //var kategori = vt.Kategoriler.Where(i => i.Id == 1).FirstOrDefault();
-
-            //if (kategori != null)
-            //{
-            //    kategori.KategoriAdi = "Telefonlar";
-            //    vt.SaveChanges();
-            //}
-
-            //foreach (var item in vt.Kategoriler)
-            //{
-            //    Console.WriteLine(item.KategoriAdi);
-            //}
 
             Console.ReadLine();
         }
