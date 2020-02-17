fd0aa25c9b0f4ce885067b7d119532e65aac86e8
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-02-17 03:33:08 +0300

    Linq Filtering

--- ConsoleApp26/Program.cs
+++ ConsoleApp26/Program.cs
@@ -12,70 +12,34 @@ namespace ConsoleApp26
         {
             VTContext vt = new VTContext();
 
+            //var urun = vt.Urunler.Where(i => i.Id == 1).FirstOrDefault();
+            //var urunler = vt.Urunler.Where(i => i.KategoriId == 1).ToList();
+
+            //var urunler = vt.Kategoriler.Where(i => i.KategoriAdi == "Telefon")
+            //   .SelectMany(i => i.Urunler).ToList();
+
             var kategoriler = vt.Kategoriler
-                .Select(i => new KategoriUrunModel
+                .Where(i => i.KategoriAdi == "Bilgisayar" || i.Id == 1)
+                .Select(i => new KategoriUrunModel()
                 {
                     KategoriAdi = i.KategoriAdi,
-                    Urunler = i.Urunler.Select(j => new UrunModel
+                    Urunler = i.Urunler.Where(k => k.StokAdet > 300).Select(j => new UrunModel
                     {
                         UrunAdi = j.UrunAdi,
                         Fiyat = j.Fiyat
                     }).ToList()
-                })
-                .ToList();
+                }).ToList();
+
 
             foreach (var kategori in kategoriler)
             {
-                Console.WriteLine("Kategori: {0}\n{{", kategori.KategoriAdi);
+                Console.WriteLine("Ürün Kategori: {0}", kategori.KategoriAdi);
                 foreach (var urun in kategori.Urunler)
                 {
-                    Console.WriteLine("    {0}", urun.UrunAdi);
+                    Console.WriteLine("Ürün Adı: {0}\nÜrün Fiyatı: {1}", urun.UrunAdi, urun.Fiyat);
                 }
-                Console.WriteLine("}");
+                Console.WriteLine("------------------------");
             }
-
-
-
-
-
-
-
-
-
-
-
-
-            //var urunler = vt.Urunler.Select(i => new UrunModel()
-            //{
-            //    UrunAdi = i.UrunAdi.Length > 12 ? i.UrunAdi.Substring(0,10) + "..." : i.UrunAdi,
-            //    Fiyat = i.Fiyat,
-            //    Kategori = i.Kategori.KategoriAdi
-            //}).ToList();
-
-            //foreach (var urun in urunler)
-            //{
-            //    Console.WriteLine("ürün adı: {0}\nürün fiyatı: {1}\nürün kategorisi: {2}", urun.UrunAdi, urun.Fiyat, urun.Kategori);
-            //    Console.WriteLine("------------------------------------------------------");
-            //}
-
-
-
-
-            //var urunler = vt.Urunler.Select(i => new
-            //{
-            //    // anonymous object
-            //    i.UrunAdi,
-            //    i.Fiyat,
-            //    i.Kategori
-            //})
-            //.ToList();
-            //var kategoriler = vt.Kategoriler.ToList();
-
-            //foreach (var urun in urunler)
-            //{
-            //    Console.WriteLine("Ürün adı: {0}\nÜrün Fiyatı: {1}\nÜrün Kategorisi: {2}", urun.UrunAdi, urun.Fiyat, urun.Kategori.KategoriAdi);
-            //    Console.WriteLine("------------------------------------------------------");
-            //}
 
             Console.ReadLine();
         }
